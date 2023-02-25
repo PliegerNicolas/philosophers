2b004c39da56b80bfdd5ec3c68b988986cd5acac
PliegerNicolas <plieger05@gmail.com>
2023-02-25 13:37:15 +0100

    Functionnal but not normed and problems with helgrind detected.

--- philo/srcs/life/routine.c
+++ philo/srcs/life/routine.c
@@ -6,7 +6,7 @@
 /*   By: nicolas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 15:34:19 by nicolas           #+#    #+#             */
-/*   Updated: 2023/02/25 13:11:15 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/02/25 13:36:21 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "philosophers.h"
@@ -37,7 +37,8 @@ t_bool	try_ending(t_philosopher *philosopher, t_rules *rules)
 	}
 	pthread_mutex_unlock(&philosopher->last_meal_mutex);
 	pthread_mutex_unlock(&rules->end_mutex);
-	return (philosopher->end | rules->end);
+	//return (philosopher->end | rules->end);
+	return (philosopher->end);
 }
 
 void	try_thinking(t_philosopher *philosopher, t_rules *rules)
@@ -46,6 +47,52 @@ void	try_thinking(t_philosopher *philosopher, t_rules *rules)
 		return ;
 	put_philosopher_action(philosopher, thinking);
 	philosopher->status = thinking;
+}
+
+void	try_grabbing_forks(t_philosopher *philosopher, t_rules *rules)
+{
+	if (philosopher->status != thinking || try_ending(philosopher, rules))
+		return ;
+	pthread_mutex_lock(&philosopher->left_fork);
+	put_philosopher_action(philosopher, grabbing_fork);
+	if (rules->total_philos > 1)
+	{
+		pthread_mutex_lock(philosopher->right_fork);
+		put_philosopher_action(philosopher, grabbing_fork);
+	}
+	else
+		usleep(rules->time_to_die * 1000);
+	philosopher->status = grabbing_fork;
+}
+
+void	try_eating(t_philosopher *philosopher, t_rules *rules)
+{
+	if (philosopher->status != grabbing_fork || try_ending(philosopher, rules))
+	{
+		pthread_mutex_unlock(&philosopher->left_fork);
+		pthread_mutex_unlock(philosopher->right_fork);
+		return ;
+	}
+	put_philosopher_action(philosopher, eating);
+	philosopher->status = eating;
+	usleep(rules->time_to_eat * 1000);
+	pthread_mutex_unlock(&philosopher->left_fork);
+	pthread_mutex_unlock(philosopher->right_fork);
+	if (try_ending(philosopher, rules))
+		return ;
+	pthread_mutex_lock(&philosopher->last_meal_mutex);
+	philosopher->last_meal = get_time();
+	philosopher->meals++;
+	pthread_mutex_unlock(&philosopher->last_meal_mutex);
+}
+
+void	try_sleeping(t_philosopher *philosopher, t_rules *rules)
+{
+	if (philosopher->status != eating || try_ending(philosopher, rules))
+		return ;
+	put_philosopher_action(philosopher, sleeping);
+	philosopher->status = sleeping;
+	usleep(rules->time_to_sleep * 1000);
 }
 
 void	*philosopher_routine(void *ptr)
@@ -59,6 +106,9 @@ void	*philosopher_routine(void *ptr)
 	while (!try_ending(philosopher, rules))
 	{
 		try_thinking(philosopher, rules);
+		try_grabbing_forks(philosopher, rules);
+		try_eating(philosopher, rules);
+		try_sleeping(philosopher, rules);
 	}
 	return (NULL);
 }
